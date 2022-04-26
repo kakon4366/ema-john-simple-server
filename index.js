af20e90e9016d099f17bf60e542e565d5b861528
kakon4366 <kakonbarman4366@gmail.com>
2022-04-26 17:47:39 +0600

    ema john order review data post success

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lry7n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
@@ -46,6 +46,17 @@ async function run() {
 		app.get("/productCount", async (req, res) => {
 			const count = await productCollection.estimatedDocumentCount();
 			res.send({ count });
+		});
+
+		// use post to get products by ids
+		app.post("/productByKeys", async (req, res) => {
+			const keys = req.body;
+			const ids = keys.map((id) => ObjectId(id));
+			const query = { _id: { $in: ids } };
+			const cursor = productCollection.find(query);
+			const products = await cursor.toArray();
+
+			res.send(products);
 		});
 	} finally {
 		// await client.close();
