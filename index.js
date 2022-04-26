7091aae300cc76691fbd2c1c131a08ff1f442046
kakon4366 <kakonbarman4366@gmail.com>
2022-04-26 16:29:34 +0600

    Middleware added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lry7n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -22,17 +23,28 @@ async function run() {
 			.collection("products");
 
 		app.get("/products", async (req, res) => {
+			const page = parseInt(req.query.page);
+			const size = parseInt(req.query.size);
+
 			const query = {};
 			const cursor = productCollection.find(query);
-			const products = await cursor.toArray();
+
+			let products;
+			if (page || size) {
+				products = await cursor
+					.skip(page * size)
+					.limit(size)
+					.toArray();
+			} else {
+				products = await cursor.toArray();
+			}
+
 			res.send(products);
 		});
 
 		//product count
 		app.get("/productCount", async (req, res) => {
-			const query = {};
-			const cursor = productCollection.find(query);
-			const count = await cursor.count();
+			const count = await productCollection.estimatedDocumentCount();
 			res.send({ count });
 		});
 	} finally {
